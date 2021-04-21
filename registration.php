f66882a1ae5b41cd85da50e5c34b5f47c527fcac
MagEx Digital <hello@magexdigital.com>
2021-04-21 16:05:14 +0700

    Init commit

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Hidro Le.
+ * Job Title: Magento Developer
+ * Project Name: m2cedefault.local
+ * Date: 21/04/2021
+ * Time: 15:38
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MagEx_Base', __DIR__);
